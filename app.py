c6a8823b0a27d41302ace774b57793a749811bc9
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-17 23:49:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ def index():
     if request.method == 'POST':
         # Get the search query from the form
         search_query = request.form.get('search_query')
-        
-        # Process the search query and retrieve music results from the API
+
+        # Process the search query and retrieve a single music result from the API
         # Replace the code below with your actual API integration logic
-        music_results = []
-        
-        return render_template('index.html', music_results=music_results)
-    
+        music_result = None
+
+        return render_template('index.html', music_result=music_result)
+
     return render_template('index.html')
 
 if __name__ == '__main__':
