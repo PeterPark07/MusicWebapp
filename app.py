25d1482ea654adbdb65a68cc3e6568665ede7357
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-18 00:08:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 from flask import Flask, render_template, request
+from main import search, download
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Get the search query from the form
         search_query = request.form.get('search_query')
-
-        # Process the search query and retrieve a single music result from the API
-        # Replace the code below with your actual API integration logic
-        music_result = None
-
+        music_result = search(search_query)
         return render_template('index.html', music_result=music_result)
 
     return render_template('index.html')
+
+# Other routes and functions
 
 if __name__ == '__main__':
     app.run()
