ef6c6dfa65fda895fe7f772b055f70ba1dece970
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-18 01:39:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_file
 from main import search, download_audio
 
 app = Flask(__name__)
+app.config['TIMEOUT'] = 600  # Set the timeout to 10 minutes (600 seconds)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
