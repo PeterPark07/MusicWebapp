a7f45e2496c9a0c9dd39f4760ee3eff473f037d8
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-18 16:40:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         search_query = request.form.get('search_query')
-        music_result = search(search_query, 5)
-        return render_template('index.html', music_result=music_result)
+        music_result, youtube_url = search(search_query )
+        download_url = f'/download?url={youtube_url}'
+        return render_template('index.html', music_result=music_result, youtube_url=youtube_url, download_url=download_url)
 
     return render_template('index.html')
 
