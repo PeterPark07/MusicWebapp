9981554fd5f1b0e94b35f33999b4cdd1574ad3aa
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-20 17:30:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, send_file
-from main import search,show_audio, download_audio
+from main import search, download_audio
 
 app = Flask(__name__)
 
@@ -15,20 +15,19 @@ def index():
 @app.route('/download', methods=['GET'])
 def download():
     url = request.args.get('url')
-    response,  thumbnail = show_audio(url)
+    response, filepath , thumbnail = download_audio(url)
 
     if thumbnail:
-        return render_template('response.html', audio_url=url, thumbnail=thumbnail)
+        return render_template('response.html', file=filepath, thumbnail=thumbnail)
     else:
         return render_template('response.html', message=response)
    
 @app.route('/download_file', methods=['GET'])
 def download_file():
-    url = request.args.get('url')
-    response, filepath = download_audio(url)
+    file = request.args.get('file')
 
-    if filepath:
-        return send_file(filepath, as_attachment=True)
+    if file:
+        return send_file(file, as_attachment=True)
     else:
         return render_template('response.html', message=response)
     
