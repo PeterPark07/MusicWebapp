c4a6f3a1a41c834bd613e73d30291550fc3cf474
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-20 16:50:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, send_file
 from main import search, download_audio
 
 app = Flask(__name__)
@@ -18,9 +18,19 @@ def download():
     response, audio_url , thumbnail = download_audio(url)
 
     if audio_url:
-        return redirect(audio_url)
+        return render_template('response.html', audio_url=audio_url, thumbnail=thumbnail)
     else:
         return render_template('response.html', message=response)
+   
+@app.route('/download_file', methods=['GET'])
+def download_file():
+    url = request.args.get('url')
+    response, audio_url, thumbnail = download_audio(url)
 
+    if audio_url:
+        return send_file(audio_url, as_attachment=True)
+    else:
+        return render_template('response.html', message=response)
+    
 if __name__ == '__main__':
     app.run()
